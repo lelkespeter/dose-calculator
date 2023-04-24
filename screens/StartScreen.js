c42c5374327c209c87fa6d5b54d67e1b1a9558a6
peter-swe <lelkes@me.com>
2023-04-24 23:00:21 +0200

    buttton navigation added

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,13 +1,21 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 import React from "react";
 import WeightInput from "../components/WeightInput";
 import SearchFilter from "../components/SearchFilter";
 
-const StartScreen = () => {
+const StartScreen = ({navigation}) => {
+  function goToHandler() {
+    navigation.navigate("Kategorier");
+  }
   return (
     <>
       <WeightInput />
       <SearchFilter />
+      <View>
+        <Pressable style={{padding: 17}} onPress={goToHandler}>
+          <Text style={{color: "white"}}>Eller, visa Kategorier</Text>
+        </Pressable>
+      </View>
     </>
   );
 };
